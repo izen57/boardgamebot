d24cb73479991833e353e3efb59a66ca9f8215e0
izen57 <golovoslon@gmail.com>
2022-08-10 13:39:39 +0300

    + ещё

--- BoardGameBot/BoardGameBot/Models/ErrorViewModel.cs
+++ BoardGameBot/BoardGameBot/Models/ErrorViewModel.cs
@@ -1,5 +1,7 @@
-namespace BoardGameBot.Models {
-	public class ErrorViewModel {
+namespace BoardGameBot.Models
+{
+	public class ErrorViewModel
+	{
 		public string? RequestId { get; set; }
 
 		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
